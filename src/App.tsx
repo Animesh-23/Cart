733475fd56ea10b7016b76429b4cc9ed72c3edde
Animesh <animeshyesugade2002@gmail.com>
2023-09-19 21:03:15 +0530

    finished logic

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,68 @@ import Phone3 from "./assets/phone-3.png";
 import Phone4 from "./assets/phone-4.png";
 import { useState } from "react";
 const App = () => {
-  const [cartItems, setCartItems] = useState(4);
+  const [cartItems, setCartItems] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [clear, setClear] = useState(false);
+  const updateTotalCost = (newItemPrice: number, increDecre: number) => {
+    setCartItems((cart) => {
+      return cart + increDecre;
+    });
+    setTotal((total) => {
+      return total + newItemPrice;
+    });
+  };
   return (
     <>
       <Navbar cartItems={cartItems} />
       <div className=" w-fit mx-auto text-2xl font-medium mt-28">
         <h2 className=" text-3xl my-10 text-center">Your Bag</h2>
-        <div className=" flex flex-col gap-5">
-          <Product img={Phone1} title="Samsung X 8" price={5000} />
-          <Product img={Phone2} title="Google pixel" price={7000} />
-          <Product img={Phone3} title="Xiomie" price={8000} />
-          <Product img={Phone4} title="Samsung galaxy" price={6000} />
-        </div>
+
+        {!clear && (
+          <div className=" flex flex-col gap-5">
+            <Product
+              key={1}
+              img={Phone1}
+              title="Samsung X 8"
+              price={5000}
+              setTotal={updateTotalCost}
+            />
+            <Product
+              key={2}
+              img={Phone2}
+              title="Google pixel"
+              price={7000}
+              setTotal={updateTotalCost}
+            />
+            <Product
+              key={3}
+              img={Phone3}
+              title="Xiomie"
+              price={8000}
+              setTotal={updateTotalCost}
+            />
+            <Product
+              key={4}
+              img={Phone4}
+              title="Samsung galaxy"
+              price={6000}
+              setTotal={updateTotalCost}
+            />
+          </div>
+        )}
         <div className=" flex justify-between my-5">
           <span>Total</span>
-          <span>$1010</span>
+          <span>{total}</span>
         </div>
         <div className="text-center">
-          <button className=" bg-black text-white text-lg font-normal font-mono rounded-lg px-3 py-1">
+          <button
+            className=" bg-black text-white text-lg font-normal font-mono rounded-lg px-3 py-1"
+            onClick={() => {
+              setCartItems(0);
+              setTotal(0);
+              setClear(true);
+            }}
+          >
             Clear Cart
           </button>
         </div>
--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,37 +1,66 @@
 import Up from "../assets/up.svg";
 import Down from "../assets/down.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type propsType = {
   img: string;
   title: string;
   price: number;
+  setTotal: (newItemPrice: number, increDecre: number) => void;
 };
-const Product = ({ img, title, price }: propsType) => {
+const Product = ({ img, title, price, setTotal }: propsType) => {
   const [items, setItems] = useState(1);
+  let flag = true;
+  useEffect(() => {
+    if (flag) {
+      setTotal(price, items);
+      flag = false;
+    }
+  }, []);
   return (
-    <div className=" m-auto w-full flex items-center justify-between gap-4 ">
-      <img src={img} width={"70px"} height={"70px"} />
-      <div className="w-96">
-        <p>{title}</p>
-        <p>{price}</p>
-        <span className=" text-blue-700 text-sm">remove</span>
+    items >= 1 && (
+      <div className=" m-auto w-full flex items-center justify-between gap-7 ">
+        <img src={img} width={"70px"} height={"70px"} />
+        <div className="w-96 text-lg">
+          <p>{title}</p>
+          <p>{price}</p>
+          <span
+            className=" text-blue-700 text-sm cursor-pointer"
+            onClick={() => {
+              setItems(0);
+              setTotal(-1 * price * items, -1 * items);
+            }}
+          >
+            remove
+          </span>
+        </div>
+        <div className=" flex flex-col items-center">
+          <img
+            src={Up}
+            width={"30px"}
+            height={"30px"}
+            onClick={() => {
+              setItems((items) => {
+                return items + 1;
+              });
+              setTotal(price, 1);
+            }}
+          />
+          <span>{items}</span>
+          <img
+            src={Down}
+            width={"30px"}
+            height={"30px"}
+            onClick={() => {
+              setItems((items) => {
+                return items - 1;
+              });
+              setTotal(-1 * price, -1);
+            }}
+          />
+        </div>
       </div>
-      <div className=" flex flex-col items-center">
-        <img
-          src={Up}
-          width={"30px"}
-          height={"30px"}
-          onClick={() => {
-            setItems((items) => {
-              return items + 1;
-            });
-          }}
-        />
-        <span>{items}</span>
-        <img src={Down} width={"30px"} height={"30px"} />
-      </div>
-    </div>
+    )
   );
 };
 
